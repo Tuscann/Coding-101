7210593b7fc7806c50137df26675b4e2612869d2
Tuscann <fbinnzhivko@gmail.com>
2016-03-11 13:02:40 +0200

    :)
    :)

---
+++ 02.Simple Calculation/10.RadiansToDegrees/10.RadiansToDegrees.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var radians = double.Parse(Console.ReadLine());
+        var gradus = radians * 180 / Math.PI;
+        Console.WriteLine("{0}", Math.Round(gradus));
+    }
+}
+
