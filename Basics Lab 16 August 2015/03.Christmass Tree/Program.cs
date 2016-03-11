a01e1eff8a6e30494737154ec02fc3f7d6af70f5
Tuscann <fbinnzhivko@gmail.com>
2016-03-11 13:09:02 +0200

    :)
    :)

---
+++ Basics Lab 16 August 2015/03.Christmass Tree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            //http://pastebin.com/JXbHv48N
+        }
+        for (int i = 0; i < n; i++)
+        {
+
+        }
+        for (int i = 0; i < n; i++)
+        {
+            //char xdsa ="'";
+            Console.WriteLine("{0}{1}{2}{0}", new string('\'', n - 1), new string('\'', n - 1), new string('\'', n - 1));
+        }
+        Console.WriteLine(new string('_', 2 * n + 1));
+
+
+    }
+    //a| |a
+}
+
---
+++ Basics Lab November 2014/01.Account Information/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var firstName = Console.ReadLine();
+        var lastName = Console.ReadLine();
+        var id = int.Parse(Console.ReadLine());
+        var totalBalance = double.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Hello, {0} {1}", firstName, lastName);
+        Console.WriteLine("Client id: {0}", id);
+        Console.WriteLine("Total balance: {0:f2}", totalBalance);
+        if (totalBalance >= 0) { Console.WriteLine("Active: yes"); }
+        else { Console.WriteLine("Active: no"); }
+
+    }
+}
+
