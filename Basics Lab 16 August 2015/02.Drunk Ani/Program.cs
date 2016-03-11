7f49ce6ce5f57988e7ee0370ed6c780a10c5930d
Tuscann <fbinnzhivko@gmail.com>
2016-03-11 13:12:09 +0200

    :)

---
+++ Basics Lab 16 August 2015/02.Drunk Ani/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double numbersOfCabins = double.Parse(Console.ReadLine());
+        double s = double.Parse(Console.ReadLine());
+
+    }
+}
+
