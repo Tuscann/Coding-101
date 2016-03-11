59752990fdddfc2e8e26ed9c312597748d85d8fe
Tuscann <fbinnzhivko@gmail.com>
2016-03-11 13:04:40 +0200

    :)
    :)

---
+++ 02.Simple Calculation/08.TriangleArea/08.TriangleArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class TriangleArea
+{
+    static void Main()
+    {
+        var a = double.Parse(Console.ReadLine());
+        var h = double.Parse(Console.ReadLine());
+
+        var area = a*h/2;
+
+        Console.WriteLine( "Triangle area = {0:0.00}",area);
+    }
+}
+
