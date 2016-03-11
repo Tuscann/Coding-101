d61a09a9c77227296957639e827cf9af02490e11
Tuscann <fbinnzhivko@gmail.com>
2016-03-11 13:10:21 +0200

    :)

---
+++ Basics Exam 26 August 2014/01. Tic-Tac-Toe Power/01. Tic-Tac-Toe Power.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int value = int.Parse(Console.ReadLine());
+
+        int index = y * 3 + x + 1;
+        int trueValue = value + y * 3 + x;
+
+
+        Console.WriteLine((ulong)Math.Pow(trueValue, index));
+
+        //if (x == 0 && y == 0)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value, 1));
+        //}
+        //else if (x == 0 && y == 1)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 3, 4));
+        //}
+        //else if (x == 0 && y == 2)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 6, 7));
+        //}
+        //else if (x == 1 && y == 0)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 1, 2));
+        //}
+        //else if (x == 1 && y == 1)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 4, 5));
+        //}
+        //else if (x == 1 && y == 2)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 7, 8));
+        //}
+        //else if (x == 2 && y == 0)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 2, 3));
+        //}
+        //else if (x == 2 && y == 1)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 5, 6));
+        //}
+        //else if (x == 2 && y == 2)
+        //{
+        //    Console.WriteLine((ulong)Math.Pow(value + 8, 9));
+        //}
+    }
+}
+
