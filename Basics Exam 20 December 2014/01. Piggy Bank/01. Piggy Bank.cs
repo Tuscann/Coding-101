2ae3646ac37e9314b33be106d2ca6d1debeb4680
Tuscann <fbinnzhivko@gmail.com>
2016-03-12 16:18:59 +0200

    :)

---
+++ Basics Exam 20 December 2014/01. Piggy Bank/01. Piggy Bank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        double price = double.Parse(Console.ReadLine());
+        double partyDays = double.Parse(Console.ReadLine());
+
+        double savings = ((30 - partyDays) * 2) - (5 * partyDays);
+        //Console.WriteLine(monthlyBalance);
+
+        if (savings < 0) { Console.WriteLine("never");}
+        else
+        {
+            double neededMonhts = Math.Ceiling(price / savings);
+
+            //Console.WriteLine(years);
+            double years  = neededMonhts / 12;
+
+            //Console.WriteLine(monts );
+            double mounts = neededMonhts % 12;
+
+            //Console.WriteLine(monts);
+            Console.WriteLine("{0:f0} years, {1:f0} months",Math.Floor(years), mounts);
+        }
+    }
+}
+
---
+++ Basics Exam 20 December 2014/02. Cheat Sheet/02. Cheat Sheet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        long rows = long.Parse(Console.ReadLine());
+        long columns = long.Parse(Console.ReadLine());
+
+        long rowNumber = long.Parse(Console.ReadLine());
+        long colummNumber = long.Parse(Console.ReadLine());
+
+
+        for (long row = rowNumber; row < rows + rowNumber; row++)
+        {
+            for (long col = colummNumber; col < columns + colummNumber; col++)
+            {
+                Console.Write(row * col);
+                if (col < colummNumber + columns)
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
+
