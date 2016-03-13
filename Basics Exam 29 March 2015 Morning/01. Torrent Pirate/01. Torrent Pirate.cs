9b2f9016d8790260fa42d99c1026bed46b2c4f1c
Tuscann <fbinnzhivko@gmail.com>
2016-03-13 03:19:22 +0200

    :)

---
+++ Basics Exam 29 March 2015 Morning/01. Torrent Pirate/01. Torrent Pirate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        double downloadData = double.Parse(Console.ReadLine());
+        int cinemaPrice = int.Parse(Console.ReadLine());
+        int wifeSpendingPerHour = int.Parse(Console.ReadLine());
+
+        const int DownloadSpeed = 2;
+        const int MovieSize = 1500;
+
+        double dowloadTimeInSeconds = (downloadData / DownloadSpeed) / (60*60);
+        double pricePerDowload = dowloadTimeInSeconds * wifeSpendingPerHour;
+        double numberMovieDownload = downloadData / MovieSize;
+        double costAtCinema = numberMovieDownload * cinemaPrice;
+
+
+        if (pricePerDowload > costAtCinema)
+        {
+            Console.WriteLine("cinema -> {0:f2}lv", costAtCinema);
+        }
+        else if (pricePerDowload <= costAtCinema)
+        {
+            Console.WriteLine("mall -> {0:f2}lv", pricePerDowload);
+        }
+
+    }
+}
+
---
+++ Basics Exam 29 March 2015 Morning/05. Game of Bits/05. Game of Bits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+
+
+
+
+
+
+    }
+}
+
