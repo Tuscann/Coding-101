534eab79a238fa2f3f83fad2336e1e26a737d153
Tuscann <fbinnzhivko@gmail.com>
2016-03-13 10:15:16 +0200

    :)

---
+++ Programming Basics Exam 29 March 2015 Even/01.Currency Check/01.Currency Check.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        uint rubles = uint.Parse(Console.ReadLine());
+        uint dollars = uint.Parse(Console.ReadLine());
+        uint evro = uint.Parse(Console.ReadLine());
+        uint levaOfferB = uint.Parse(Console.ReadLine());
+        uint levaMSite = uint.Parse(Console.ReadLine());
+
+
+
+        double rubless = (rubles / (double)100) ;
+        Console.WriteLine(rubless);
+        double rublesss = rubless*(double) 3.5;
+        Console.WriteLine(rublesss);
+        double dollarss = dollars * (double )1.5;
+        Console.WriteLine(dollarss);
+        double evros = evro * (double)1.95;
+        Console.WriteLine(evros);
+        double levaOfferBB = levaOfferB / (double)2;
+        Console.WriteLine(levaOfferBB);
+        Console.WriteLine(levaMSite);
+
+        double[] numbers = new[] {rublesss, dollarss, evros, levaOfferBB, levaMSite};
+        double min = numbers.Min();
+
+
+
+
+        Console.WriteLine("{0:f2}",min);
+
+    }
+}
+
