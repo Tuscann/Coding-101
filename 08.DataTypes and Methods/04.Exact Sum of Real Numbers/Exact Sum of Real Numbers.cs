48c532178f4dd822ca083d0327168757b7c07736
Tuscann <fbinnzhivko@gmail.com>
2016-03-15 13:02:36 +0200

    :)

---
+++ 08.DataTypes and Methods/04.Exact Sum of Real Numbers/Exact Sum of Real Numbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class dasd
+{
+    static void Main()
+    {
+
+      double numbers = double.Parse(Console.ReadLine());
+        
+      decimal sum = 0.0m;
+      for (double i = 0; i < numbers; i++)
+      {
+        sum = sum + decimal.Parse(Console.ReadLine());
+      }
+       Console.WriteLine("{0}",sum);
+
+    }
+}
+
+
