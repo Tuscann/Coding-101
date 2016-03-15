6ce0dd8165d79cdc8caa0f1a4f029fcecf8dbbfd
Tuscann <fbinnzhivko@gmail.com>
2016-03-16 00:12:35 +0200

    :)

---
+++ 06 March 2016/04. Histogram/04. Histogram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int p1 = 0;
+        int p2 = 0;
+        int p3 = 0;
+        int p4 = 0;
+        int p5 = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            var num = int.Parse(Console.ReadLine());
+                 if (num < 200) { p1++; }
+            else if (num < 400) { p2++; }
+            else if (num < 600) { p3++; }
+            else if (num < 800) { p4++; }
+            else if (num <= 1000) { p5++; }
+        }
+        var p1Percentage = p1 * 100d / n;
+        var p2Percentage = p2 * 100d / n;
+        var p3Percentage = p3 * 100d / n;
+        var p4Percentage = p4 * 100d / n;
+        var p5Percentage = p5 * 100d / n;
+
+
+        Console.WriteLine("{0:f2}%", p1Percentage);
+        Console.WriteLine("{0:f2}%", p2Percentage);
+        Console.WriteLine("{0:f2}%", p3Percentage);
+        Console.WriteLine("{0:f2}%", p4 * 100d / n);
+        Console.WriteLine("{0:f2}%", p5 * 100d / n);
+    }
+}
+
