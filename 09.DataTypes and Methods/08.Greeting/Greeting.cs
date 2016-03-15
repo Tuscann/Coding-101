b56e29827a4b3b584b0fae4b3bcde389bf417eb9
Tuscann <fbinnzhivko@gmail.com>
2016-03-16 01:12:36 +0200

    :)

---
+++ 09.DataTypes and Methods/08.Greeting/Greeting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Greeting
+{
+    static void Main()
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        var age = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Hello, {0} {1}.\nYou are {2} years old.", firstName, lastName, age);
+        Console.WriteLine($"Hello, {firstName} {lastName}.\r\nYou are {age} years old.");
+    }
+}
+
---
+++ 09.DataTypes and Methods/11.Filled Square/Filled Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        var number = int.Parse(Console.ReadLine());
+
+        PrintFooter(number);
+        EasyWay (number);
+        PrintFooter(number);
+
+    }
+    private static void EasyWay(int number)
+    {
+        for (var i = 1; i < number; i++)
+        {
+            Console.Write('-');
+            for (var j = 0; j <= number - 2; j++)
+            {
+                Console.Write(@"\/");
+            }
+            Console.Write('-');
+            Console.WriteLine();
+        }
+    }
+    private static void PrintFooter(int number)
+    {
+        Console.WriteLine("{0}", new string('-', 2 * number));
+    }
+
+    static void dasdas(int number)
+    {
+    }
+
+}
+
