185fccf6c58705584bc14528c0cd0e9b88fb4735
Tuscann <fbinnzhivko@gmail.com>
2016-03-19 10:48:08 +0200

    :)

---
+++ 10.Arrays/02.Prime Numbers/02.Prime Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        bool[] primes = new bool[n + 1];
+        for (int i = 0; i < n; i++)
+        {
+            primes[i] = true;
+        }
+        primes[0] = primes[1] = false;
+        
+        for (int p = 2; p < n; p++)
+        {
+            if (primes[p])
+            {
+                Fillprimes(primes, p);
+            }
+        }
+        Console.WriteLine(primes);
+
+        
+
+
+    }
+    static void Fillprimes(bool[] primes, int step)
+    {
+        for (int i = 2 * step; i < primes.Length; i += step)
+        {
+            primes[i] = false;
+        }
+    }
+}
+
