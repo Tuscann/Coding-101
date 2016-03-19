fcd1fe6c765e8290e86dbab28b65861df3fac8ba
Tuscann <fbinnzhivko@gmail.com>
2016-03-20 00:38:04 +0200

    :)

---
+++ Solution1/02. Simple Loops/02. Simple Loops.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+class Program
+{
+    static void Main()
+    {
+        BigInteger a1 = new BigInteger(int.Parse(Console.ReadLine()));
+        BigInteger a2 = new BigInteger(int.Parse(Console.ReadLine()));
+        BigInteger a3 = new BigInteger(int.Parse(Console.ReadLine()));
+        BigInteger n = new BigInteger(int.Parse(Console.ReadLine()));
+        
+        if (n==1){Console.WriteLine(a1);}
+        else if(n == 2){Console.WriteLine(a2);}
+        else if (n == 3){Console.WriteLine(a3);}
+        else
+        {
+            BigInteger total = 0;
+            for (int i = 4; i <= n; i++)
+            {
+                total = a1 + a2 + a3;
+                a1 = a2;
+                a2 = a3;
+                a3 = total;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
+
---
+++ Solution1/03. Draw Figure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("{0}",new string('*',n));
+        for (int i = 0; i <= n / 2; i++)
+        { Console.WriteLine("{0}{1}{0}", new string('.', n - 2*i),new string('*',i)); }
+
+        Console.WriteLine("{0}*{0}",new string('.',n/2));
+
+        //for (int i = 1; i < n / 2; i++)
+        //{ Console.WriteLine(".{0}.", new string('*', n - 2)); }
+        //Console.WriteLine("{0}", new string('*', n));
+
+
+    }
+}
+
