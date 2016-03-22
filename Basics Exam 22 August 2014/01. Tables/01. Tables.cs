d3e44a0d2a9ff32fe8a784c37c86dbbd21eab773
Tuscann <fbinnzhivko@gmail.com>
2016-03-22 16:46:00 +0200

    :)

---
+++ Basics Exam 22 August 2014/01. Tables/01. Tables.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        long budle1 = long.Parse(Console.ReadLine());
+        long budle2 = long.Parse(Console.ReadLine());
+        long budle3 = long.Parse(Console.ReadLine());
+        long budle4 = long.Parse(Console.ReadLine());
+        int tableTops = int.Parse(Console.ReadLine());
+        int neededTables = int.Parse(Console.ReadLine());
+
+        long legs = 1 * budle1 + 2 * budle2 + 3 * budle3 + 4 * budle4;
+        long legsLeft = legs - tableTops * 4;  ///da se izpozlava
+        long legsPossibleTables = legs / 4;
+        long possibleTables = Math.Abs((int)(legsPossibleTables) - tableTops);
+        
+        if (legsPossibleTables > tableTops){possibleTables = legsPossibleTables - possibleTables;}
+        else if (tableTops > legsPossibleTables){possibleTables = tableTops - possibleTables;}
+        else{possibleTables = tableTops;}
+        
+        long tablesNeed = possibleTables - neededTables;
+        long legsneed = legsPossibleTables - neededTables;
+        long topsneed = tableTops - neededTables;
+
+        if (tablesNeed == 0){Console.WriteLine("Just enough tables made: {0}", neededTables);}
+        else if (possibleTables > neededTables)
+        {
+            Console.WriteLine("more: {0} ", Math.Abs(tablesNeed));
+            Console.WriteLine("tops left: {0}, legs left: {1}", Math.Abs(topsneed), Math.Abs(legsneed * 4));
+        }
+
+        else // if (need > possibleTables)
+        {
+            Console.WriteLine("less: {0} ", tablesNeed);
+            Console.WriteLine("tops needed: {0}, legs needed: {1}", Math.Abs(topsneed), Math.Abs(legsneed * 4));
+        }
+
+
+    }
+}
+
+
+
---
+++ Basics Exam 22 August 2014/02. Book Orders/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Book_Orders
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            int totalBooks = 0;
+            double totalPrice = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                int packets = int.Parse(Console.ReadLine());
+                int books = int.Parse(Console.ReadLine());
+                double price = double.Parse(Console.ReadLine());
+
+                int allBooks = packets * books;
+                totalBooks = totalBooks + allBooks;
+                double discount = 0.0;
+
+                if (packets >= 10 && packets <= 19) { discount = 0.05; }
+                else if (packets >= 20 && packets <= 29) { discount = 0.06; }
+                else if (packets >= 30 && packets <= 39) { discount = 0.07; }
+                else if (packets >= 40 && packets <= 49) { discount = 0.08; }
+                else if (packets >= 50 && packets <= 59) { discount = 0.09; }
+                else if (packets >= 60 && packets <= 69) { discount = 0.10; }
+                else if (packets >= 70 && packets <= 79) { discount = 0.11; }
+                else if (packets >= 80 && packets <= 89) { discount = 0.12; }
+                else if (packets >= 90 && packets <= 99) { discount = 0.13; }
+                else if (packets >= 100 && packets <= 109) { discount = 0.14; }
+                else if (packets >= 110) { discount = 0.15; }
+
+                double finalPrice = price*allBooks - ((allBooks * price) * discount);
+                totalPrice = totalPrice + finalPrice;
+
+
+                Console.WriteLine(totalBooks);
+                Console.WriteLine("{0:f2}", totalPrice);
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
