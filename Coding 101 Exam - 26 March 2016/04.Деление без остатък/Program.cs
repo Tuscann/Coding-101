8c697c2bb19cd5b0e0cd82d8a984ceee7c3b955f
Tuscann <fbinnzhivko@gmail.com>
2016-03-28 12:46:48 +0300

    :)

--- Coding 101 Exam - 26 March 2016/04.Деление без остатък/Program.cs
+++ Coding 101 Exam - 26 March 2016/04.Деление без остатък/Program.cs
@@ -3,36 +3,25 @@ class Program
 {
     static void Main()
     {
-
         int n = int.Parse(Console.ReadLine());
 
         int p1 = 0;
         int p2 = 0;
         int p3 = 0;
-        
+
         for (int i = 0; i < n; i++)
         {
-            decimal k = decimal.Parse(Console.ReadLine());
-            if (k % 2 == 0)
-            {
-                p1++;
-            }
-            else if (k % 3 == 0)
-            {
-                p2++;
-            }
-            else if (k % 4 == 0)
-            {
-                p3++;
-            }
-            k = decimal.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
 
-
+            if (k % 2 == 0) { p1++; }
+            if (k % 3 == 0) { p2++; }
+            if (k % 4 == 0) { p3++; }
         }
-        Console.WriteLine("{0:f2}\n{1:f2}\n{2:f2}\n", p1 / 10 * 100, p2 / 10 * 100, p3 / 10 * 100);
+        decimal x = ((decimal)p1 / (decimal)n) * 100;
+        decimal x1 = ((decimal)p2 / (decimal)n) * 100;
+        decimal x2 = ((decimal)(p3) / n) * 100;
 
-
-
+        Console.WriteLine("{0:f2}%\n{1:f2}%\n{2:f2}%\n", x, x1, x2);
     }
 }
 
---
+++ Coding 101 Exam - 26 March 2016/05. Пеперуда/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+        if (n == 3)
+        {
+            Console.WriteLine("*\\ /*");
+            Console.WriteLine("{0}@{0}", new string(' ', (2 * n - 2) / 2));
+            Console.WriteLine("*/ \\*");
+        }
+        else if (n == 5)
+        {
+            Console.WriteLine("{0}\\ /{0}", new string('*', (2 * n - 4) / 2));
+            for (int i = 0; i < (n - 2) / 2; i++)
+            {
+                Console.WriteLine("{0}\\ /{0}", new string('-', (2 * n - 4) / 2));
+
+            }
+            Console.WriteLine("{0}\\ /{0}", new string('*', (2 * n - 4) / 2));
+            Console.WriteLine("{0}@{0}", new string(' ', (2 * n - 2) / 2));
+            Console.WriteLine("{0}/ \\{0}", new string('*', (2 * n - 4) / 2));
+            for (int i = 0; i < (n - 2) / 2; i++)
+            {
+                Console.WriteLine("{0}/ \\{0}", new string('-', (2 * n - 4) / 2));
+
+            }
+            Console.WriteLine("{0}/ \\{0}", new string('*', (2 * n - 4) / 2));
+
+        }
+        else if (n >= 7)
+        {
+            Console.WriteLine("{0}\\ /{0}", new string('*', (2 * n - 4) / 2));
+            for (int i = 0; i < (n - 4) / 2; i++)
+            {
+                Console.WriteLine("{0}\\ /{0}", new string('-', (2 * n - 4) / 2));
+                Console.WriteLine("{0}\\ /{0}", new string('*', (2 * n - 4) / 2));
+
+            }
+            Console.WriteLine("{0}\\ /{0}", new string('-', (2 * n - 4) / 2));
+            Console.WriteLine("{0}\\ /{0}", new string('*', (2 * n - 4) / 2));
+            Console.WriteLine("{0}@{0}", new string(' ', (2 * n - 2) / 2));
+            Console.WriteLine("{0}/ \\{0}", new string('*', (2 * n - 4) / 2));
+
+            for (int i = 0; i < (n - 4) / 2; i++)
+            {
+                Console.WriteLine("{0}/ \\{0}", new string('-', (2 * n - 4) / 2));
+                Console.WriteLine("{0}/ \\{0}", new string('*', (2 * n - 4) / 2));
+
+            }
+            Console.WriteLine("{0}/ \\{0}", new string('-', (2 * n - 4) / 2));
+            Console.WriteLine("{0}/ \\{0}", new string('*', (2 * n - 4) / 2));
+        }
+    }
+}c
+
