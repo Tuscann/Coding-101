63b80bb676c6c05424b3e2dfaff7c55e2ddcb2d2
Tuscann <fbinnzhivko@gmail.com>
2016-04-01 00:18:22 +0300

    :)

---
+++ Basics Exam 21 February 2016/01.Hungry Garfield/Hungry Garfield.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        decimal money = decimal.Parse(Console.ReadLine());
+        decimal rate = decimal.Parse(Console.ReadLine());
+        decimal pizzaPrice = decimal.Parse(Console.ReadLine());
+        decimal lasagnaPrice = decimal.Parse(Console.ReadLine());
+        decimal sandwichPrice = decimal.Parse(Console.ReadLine());
+        decimal pizzaQuantity = decimal.Parse(Console.ReadLine());
+        decimal lasagnaQuantity = decimal.Parse(Console.ReadLine());
+        decimal sandwichQuantity = decimal.Parse(Console.ReadLine());
+
+        decimal spentForPizza = (pizzaPrice / rate) * pizzaQuantity;
+        decimal spentForLasagna = (lasagnaPrice / rate) * lasagnaQuantity;
+        decimal spentForSandwich = (sandwichPrice / rate) * sandwichQuantity;
+        decimal x = spentForLasagna + spentForPizza + spentForSandwich;
+
+        if (x <= money)
+        {
+            Console.WriteLine("Garfield is well fed, John is awesome. Money left: ${0:0.00}.", money - x);
+
+        }
+        else if (x > money)
+        {
+            Console.WriteLine("Garfield is hungry. John is a badass. Money needed: ${0:0.00}.", Math.Abs(money - x));
+        }
+
+    }
+}
+
