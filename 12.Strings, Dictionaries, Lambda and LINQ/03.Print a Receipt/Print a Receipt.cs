071a9e2385aa2a555dd62e93082ab6a757c28254
Tuscann <fbinnzhivko@gmail.com>
2016-04-02 13:06:03 +0300

    :)

---
+++ 12.Strings, Dictionaries, Lambda and LINQ/03.Print a Receipt/Print a Receipt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+       var arrays = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+        Console.WriteLine("/----------------------\\");
+        foreach (var array in arrays)
+        {
+            Console.WriteLine("|{0,21:f2} |",array);
+        }
+        Console.WriteLine("|----------------------|");
+        Console.WriteLine("| Total:{0,14:f2} |", arrays.Sum());
+        Console.WriteLine("\\----------------------/");
+    }
+}
+
---
+++ 12.Strings, Dictionaries, Lambda and LINQ/12.Strings, Dictionaries, Lambda and LINQ/Print String Letters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Strings__Dictionaries__Lambda_and_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
