48d504676d6d182a3e3633343d39fda50d639373
Tuscann <fbinnzhivko@gmail.com>
2016-04-05 21:33:45 +0300

    :)

---
+++ Basics Exam 11 April 2014 Evening/01. Cinema/01. Cinema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        string input = Console.ReadLine();
+        decimal rows = decimal.Parse(Console.ReadLine());
+        decimal columns = decimal.Parse(Console.ReadLine());
+
+        decimal total = rows * columns;
+
+        if (input == "Premiere")
+        {
+            total *= (decimal)12.00;
+        }
+        else if (input == "Normal")
+        {
+            total *= (decimal)07.50;
+        }
+        else
+        {
+            total *= (decimal)05.00;
+        }
+        Console.WriteLine("{0:f2} leva", total);
+    }
+}
+
---
+++ Basics Exam 11 April 2014 Evening/02. Illuminati/02. Illuminati.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    private static void Main()
+    {
+        string input = Console.ReadLine().ToUpper();
+
+        int numberOfVowels = 0;
+        int sumOfLetters = 0;
+
+        foreach (char letter in input)
+        {
+            if (letter == 'A')
+            {
+                numberOfVowels++;
+                sumOfLetters = sumOfLetters + letter;
+            }
+            if (letter == 'E')
+            {
+                numberOfVowels++;
+                sumOfLetters = sumOfLetters + letter;
+            }
+            if (letter == 'I')
+            {
+                numberOfVowels++;
+                sumOfLetters = sumOfLetters + letter;
+            }
+            if (letter == 'O')
+            {
+                numberOfVowels++;
+                sumOfLetters = sumOfLetters + letter;
+            }
+            if (letter == 'U')
+            {
+                numberOfVowels++;
+                sumOfLetters = sumOfLetters + letter;
+            }
+        }
+        Console.WriteLine("{0}\n{1}", numberOfVowels, sumOfLetters);
+    }
+}
+
