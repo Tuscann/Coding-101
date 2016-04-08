6c12aed937bf1cc82c9568ca2d89bfb6b1ff0e49
Tuscann <fbinnzhivko@gmail.com>
2016-04-08 16:53:15 +0300

    :)

--- 10.Arrays/11.Fold and Sum/11.Fold and Sum.cs
+++ 10.Arrays/11.Fold and Sum/11.Fold and Sum.cs
@@ -7,17 +7,18 @@ class Program
     {
 
         int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
         int k = arr.Length/4;
         var rowLeft = arr.Take(k).Reverse();
         var rowRight = arr.Reverse().Take(k);
-        
-        Console.WriteLine(string.Join("  ",rowLeft));
-        Console.WriteLine(string.Join("  ", rowRight));
 
-        //var row1 = rowLeft.Concat(rowRight).ToArray();
-        //var row2 = arr.Skip(k).Take(2 * k).ToArray();
-        //var sumArr = row1.Select((x, index) => x + row2[index]);
-        //Console.WriteLine(string.Join(" ", sumArr));
+        //Console.WriteLine(string.Join("  ",rowLeft));
+        //Console.WriteLine(string.Join("  ", rowRight));
+
+        var row1 = rowLeft.Concat(rowRight).ToArray();
+        var row2 = arr.Skip(k).Take(2 * k).ToArray();
+        var sumArr = row1.Select((x, index) => x + row2[index]);
+        Console.WriteLine(string.Join(" ", sumArr));
 
 
 
