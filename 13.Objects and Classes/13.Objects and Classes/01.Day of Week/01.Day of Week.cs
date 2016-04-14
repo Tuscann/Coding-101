c6b90e4c858291ce90b71c6d26b26f9344f2a90c
Tuscann <fbinnzhivko@gmail.com>
2016-04-15 00:19:52 +0300

    :)

---
+++ 13.Objects and Classes/13.Objects and Classes/01.Day of Week/01.Day of Week.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+class Program
+{
+    static void Main()
+    {
+        var dateAsText = Console.ReadLine();
+        var date = DateTime.ParseExact(dateAsText, "d-M-yyyy",CultureInfo.InvariantCulture);
+        Console.WriteLine(date.DayOfWeek);
+
+    }
+}
+
