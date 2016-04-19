3e25ba651c70e7dcc6486c7e1b9b6e0053da7a14
Tuscann <fbinnzhivko@gmail.com>
2016-04-19 19:48:22 +0300

    :)

---
+++ 08.C# Basics Exam 14 April 2014 Evening/04.01 Magic Car Numbers/04.01 Magic Car Numbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        char[] arr = { 'A', 'B', 'C', 'E', 'H', 'K', 'M', 'P', 'T', 'X' };
+        int magicWeight = int.Parse(Console.ReadLine());
+        int weight = 0;
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int k = 0; k < arr.Length; k++)
+            {
+                weight = ((int)arr[i] + (int)arr[k] - 128) * 10;
+                for (int a = 0; a < 10; a++)
+                {
+                    if (4 * a + 40 + weight == magicWeight)
+                    {
+                        count++;
+                    }
+
+                    for (int b = 0; b < 10; b++)
+                    {
+                        if (a != b)
+                        {
+                            if (3 * b + a + 40 + weight == magicWeight)
+                            {
+                                count++;
+                            }
+                            if (3 * a + b + 40 + weight == magicWeight)
+                            {
+                                count++;
+                            }
+                            if (2 * a + 2 * b + 40 + weight == magicWeight)
+                            {
+                                count += 3;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
---
+++ 08.C# Basics Exam 14 April 2014 Evening/05.01  Bit Flipper/05.01  Bit Flipper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class BitFlipperBitwise
+{
+    static void Main()
+    {
+        ulong number = ulong.Parse(Console.ReadLine());
+        ulong mask = 7;
+        int n = 61;
+
+        for (int i = n; i >= 0; i--)
+        {
+            ulong checker = mask << i;
+            checker = checker & number;
+            checker = checker >> i;
+            if (checker == 0 || checker == 7)
+            {
+                number = number ^ (mask << i);
+                i -= 2;
+            }
+        }
+        Console.WriteLine(number);
+    }
+}
