596066996b96bb0b9bf62716e1fadf2e21239ec1
Tuscann <fbinnzhivko@gmail.com>
2016-04-27 17:22:24 +0300

    :)

--- 01.C# Basics Exam 10 April 2014 Morning/01. Joro The Football Player/01. Joro The Football Player.cs
+++ 01.C# Basics Exam 10 April 2014 Morning/01. Joro The Football Player/01. Joro The Football Player.cs
@@ -3,12 +3,9 @@ class Program
 {
     static void Main()
     {
-
         string input = Console.ReadLine();
-
         decimal a = decimal.Parse(Console.ReadLine());
         decimal b = decimal.Parse(Console.ReadLine());
-
 
         decimal total = (((decimal)52 - b) * ((decimal)2 / (decimal)3)) + a / (decimal)2 + b;
 
@@ -19,4 +16,3 @@ class Program
         Console.WriteLine("{0:f0}", Math.Floor(total));
     }
 }
-
---
+++ New folder/13.Objects and Classes/02.00 Randomize Words/02.00 Randomize Words.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        string[] words = Console.ReadLine().Split(' ');
+
+        for (int word = 0; word < words.Length - 1; word++)
+        {
+            int swapIndex = rnd.Next(word + 1, words.Length);
+            words = Swap(words, word, swapIndex);
+        }
+        //foreach (var word in words)
+        //{
+        //    Console.WriteLine(word);
+        //}
+        Console.WriteLine(string.Join("\r\n", words));
+    }
+    private static string[] Swap(string[] words, int index1, int index2)
+    {
+        string temp = words[index1];
+        words[index1] = words[index2];
+        words[index2] = temp;
+
+        return words;
+    }
+}
