b6b06d785e58f283c0c47d64f21dea765aa2caf8
Nedqlkov <fbinnzhivko@gmail.com>
2017-05-08 18:36:39 +0300

    Small fixes with pdf files

---
+++ 41.Programming Basics Exam - 07 May 2017/1/1/1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace _1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double first = double.Parse(Console.ReadLine());
+            double secound = double.Parse(Console.ReadLine());
+            double third = double.Parse(Console.ReadLine());
+            double fourt = double.Parse(Console.ReadLine());
+            double five = double.Parse(Console.ReadLine());
+
+            double rakia = fourt * (first / 2);
+            double vino = third * (first / 2 - (0.4 * first / 2));
+            double bira = secound * (first / 2 - (0.8 * first / 2));
+            double uuskii = first * five;
+
+            Console.WriteLine("{0:f2}", rakia + vino + bira + uuskii);
+        }
+    }
+}
--- 41.Programming Basics Exam - 07 May 2017/1/6/6.cs
+++ 41.Programming Basics Exam - 07 May 2017/1/6/6.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _6
+namespace Exam1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int M = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine());
+            int L = int.Parse(Console.ReadLine());
+            int specialNumber = int.Parse(Console.ReadLine());
+            int checkNumber = int.Parse(Console.ReadLine());
+
+            int number = 0;
+            for (int i = M; i >= 1; i--)
+            {
+                for (int j = N; j >= 1; j--)
+                {
+                    for (int k = L; k >= 1; k--)
+                    {
+                        number = 100 * i + 10 * j + k;
+                        if (number % 3 == 0)
+                        {
+                            specialNumber += 5;
+                        }
+                        else if (number % 10 == 5)
+                        {
+                            specialNumber -= 2;
+                        }
+                        else if (number % 2 == 0)
+                        {
+                            specialNumber *= 2;
+                        }
+                        if (specialNumber >= checkNumber)
+                        {
+                            Console.WriteLine("Yes! Control number was reached! Current special number is {0}.", specialNumber);
+                            return;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("No! {0} is the last reached special number.", specialNumber);
         }
     }
 }
