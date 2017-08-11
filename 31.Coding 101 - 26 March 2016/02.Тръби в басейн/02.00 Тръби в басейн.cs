b6a94087521637ebba2174305a52daf0c1e3b6cb
Nedqlkov <fbinnzhivko@gmail.com>
2017-08-11 15:47:58 +0300

    new tasks added

--- 31.Coding 101 - 26 March 2016/02.Тръби в басейн/02.00 Тръби в басейн.cs
+++ 31.Coding 101 - 26 March 2016/02.Тръби в басейн/02.00 Тръби в басейн.cs
@@ -7,7 +7,7 @@ class Program
         double P1 = double.Parse(Console.ReadLine());
         double P2 = double.Parse(Console.ReadLine());
         double H = double.Parse(Console.ReadLine());
-        
+
         double first = P1 * H;
         double secound = P2 * H;
         double total = first + secound;
@@ -15,17 +15,16 @@ class Program
         if (total <= v)
         {
             double prozent = total / (v / 100);
-            
             double prozentfirst = first / (total / 100);
             double prozentsecound = secound / (total / 100);
 
             Console.WriteLine("The pool is {0}% full. Pipe 1: {1:f0}%. Pipe 2: {2:f0}%.", (int)prozent, (int)prozentfirst, (int)prozentsecound);
         }
-        else if (total >v)
+        else if (total > v)
         {
             double more = total - v;
 
-            Console.WriteLine("For {0} hours the pool overflows with {1} liters.",H,more);
+            Console.WriteLine("For {0} hours the pool overflows with {1:f1} liters.", H, more);
         }
     }
 }
--- 39.Programming Basics Exam - 19 March 2017 - Morning/3/Program.cs
+++ 39.Programming Basics Exam - 19 March 2017 - Morning/3/Program.cs
@@ -1,52 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _3
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        double budjet = double.Parse(Console.ReadLine());
+        string season = Console.ReadLine();
+        
+        if (season == "Summer")
         {
-            double budjet = double.Parse(Console.ReadLine());
-            string season = Console.ReadLine();
-
-
-            if (season == "Summer")
+            if (budjet <= 1000)
             {
-
-                if (budjet <= 1000)
-                {
-                    Console.WriteLine("Alaska - Camp - {0:f2}",0.65*budjet);
-                }
-                else if (budjet > 1000 && budjet <= 3000)
-                {
-                    Console.WriteLine("Alaska - Hut - {0:f2}", 0.80 * budjet);
-                }
-                else if (budjet > 3000)
-                {
-                    Console.WriteLine("Alaska - Hotel - {0:f2}", 0.90 * budjet);
-                }
-
+                Console.WriteLine("Alaska - Camp - {0:f2}", 0.65 * budjet);
             }
-            else if (season == "Winter")
+            else if (budjet > 1000 && budjet <= 3000)
             {
-                if (budjet <= 1000)
-                {
-                    Console.WriteLine("Morocco - Camp - {0:f2}", 0.45 * budjet);
-                }
-                else if (budjet > 1000 && budjet <= 3000)
-                {
-                    Console.WriteLine("Morocco - Hut - {0:f2}", 0.60 * budjet);
-                }
-                else if (budjet > 3000)
-                {
-                    Console.WriteLine("Morocco - Hotel - {0:f2}", 0.90 * budjet);
-                }
+                Console.WriteLine("Alaska - Hut - {0:f2}", 0.80 * budjet);
             }
-
+            else if (budjet > 3000)
+            {
+                Console.WriteLine("Alaska - Hotel - {0:f2}", 0.90 * budjet);
+            }
+        }
+        else if (season == "Winter")
+        {
+            if (budjet <= 1000)
+            {
+                Console.WriteLine("Morocco - Camp - {0:f2}", 0.45 * budjet);
+            }
+            else if (budjet > 1000 && budjet <= 3000)
+            {
+                Console.WriteLine("Morocco - Hut - {0:f2}", 0.60 * budjet);
+            }
+            else if (budjet > 3000)
+            {
+                Console.WriteLine("Morocco - Hotel - {0:f2}", 0.90 * budjet);
+            }
         }
     }
 }
