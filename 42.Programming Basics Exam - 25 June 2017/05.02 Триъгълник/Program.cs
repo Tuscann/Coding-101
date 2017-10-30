962a0ecae08ef275f205557022ef5f990c9ff589
Tuscann <fbinnzhivko@gmail.com>
2017-10-30 14:42:45 +0200

    new Solutions

---
+++ 42.Programming Basics Exam - 25 June 2017/05.02 Триъгълник/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int height = 4 * n + 1;
+
+        Console.WriteLine("{0}", new string('#', height));
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n / 2)
+            {
+                Console.WriteLine("{0}{1}{2}(@){2}{1}{0}",
+                    new string('.', i + 1),
+                    new string('#', height / 2 - (2 * i + 1)),
+                    new string(' ', n / 2 - 1));
+            }
+            else
+            {
+                Console.WriteLine("{0}{1}{2}{1}{3}",
+                    new string('.', i + 1),
+                    new string('#', height / 2 - (2 * i + 1)),
+                    new string(' ', 1 + i * 2),
+                    new string('.', i + 1));
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("{0}{1}{0}",
+                new string('.', n + 1 + i),
+                new string('#', height - 2 * (n + 1 + i)));
+        }
+    }
+}
---
+++ 42.Programming Basics Exam - 25 June 2017/05.03 Триъгълник/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()  // 100/100
+    {
+        int n = int.Parse(Console.ReadLine());
+        int height = 4 * n + 1;
+
+        Console.WriteLine(new string('#', height));
+        Console.WriteLine(".{0} {0}.", new string('#', 2 * n - 1));
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (i == (n - 2) / 2)
+                Console.WriteLine("{0}{1}{2}(@){2}{1}{0}",
+                    new string('.', i + 2),
+                    new string('#', 2 * n - 3 - 2 * i),
+                    new string(' ', i)
+                );
+            else
+                Console.WriteLine("{0}{1}{2} {2}{1}{0}",
+                    new string('.', i + 2),
+                    new string('#', 2 * n - 3 - 2 * i),
+                    new string(' ', i + 1));
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("{0}{1}{0}",
+                new string('.', n + 1 + i),
+                new string('#', 2 * n - 1 - 2 * i));
+        }
+    }
+}
